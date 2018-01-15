3b4871aa7b777293ee298b913536e5a1ed816981
Marcos <mail@sender.com>
2018-01-15 18:23:55 +0800

    cleaned up listservers function removing excess for loops and started working on getting available resources.

--- cloudatcost.py
+++ cloudatcost.py
@@ -5,6 +5,7 @@ from pprint import pprint
 
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 class api():
@@ -25,23 +26,46 @@ class api():
             s.post("https://panel.cloudatcost.com/manage-check2.php", headers=s.headers, data=payload)
             soup = BeautifulSoup(s.get("https://panel.cloudatcost.com").content, 'html5lib')
             server_ids = dict()
-            for i in soup.find_all("div", attrs={"class": ["panel panel-default"]}):
-                for i in i.find_all("h3", {"class": ["panel-title"]}):
-                    for i in i.find_all("tbody"):
-                        for i in i.find_all("tr"):
-                            for i in i.find_all("td"):
-                                for x in i.find_all("button", "btn btn-group-sm btn-info"):
-                                    server_ids[
-                                        str(x).split("Server ID:     &lt;/td&gt;&lt;td&gt; ")[1].rsplit("&")[0]] = {
-                                        "id": str(x).split("Server ID:     &lt;/td&gt;&lt;td&gt; ")[1].rsplit("&")[0],
-                                        "netmask": str(x).split("Netmask:&lt;/td&gt;&lt;td&gt; ")[1].rsplit("&")[0],
-                                        "ip": str(x).split("IP Address:     &lt;/td&gt;&lt;td&gt; ")[1].rsplit("&")[0],
-                                        "gateway": str(x).split("Gateway:&lt;/td&gt;&lt;td&gt; ")[1].rsplit("&")[0],
-                                        "rootpass": str(x).split("Password:&lt;/td&gt;&lt;td&gt; ")[1].rsplit("&")[0],
-                                        "mode": str(x).split("Run Mode:&lt;/td&gt;&lt;td&gt; ")[1].rsplit("&")[0]
+            for i in soup.find_all("td"):
+                for i in i.find_all("button", "btn btn-group-sm btn-info"):
+                    server_ids[
+                                        str(i).split("Server ID:     &lt;/td&gt;&lt;td&gt; ")[1].rsplit("&")[0]] = {
+                                        "id": str(i).split("Server ID:     &lt;/td&gt;&lt;td&gt; ")[1].rsplit("&")[0],
+                                        "netmask": str(i).split("Netmask:&lt;/td&gt;&lt;td&gt; ")[1].rsplit("&")[0],
+                                        "ip": str(i).split("IP Address:     &lt;/td&gt;&lt;td&gt; ")[1].rsplit("&")[0],
+                                        "gateway": str(i).split("Gateway:&lt;/td&gt;&lt;td&gt; ")[1].rsplit("&")[0],
+                                        "rootpass": str(i).split("Password:&lt;/td&gt;&lt;td&gt; ")[1].rsplit("&")[0],
+                                        "mode": str(i).split("Run Mode:&lt;/td&gt;&lt;td&gt; ")[1].rsplit("&")[0]
                                     }
         return server_ids
 
+    def getresources(self):
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, "
+                          "like Gecko) Chrome/63.0.3239.132 Safari/537.36",
+            "origin": "https://panel.cloudatcost.com",
+            "referer": "https://panel.cloudatcost.com/login.php"}
+        payload = {"username": self.email, "password": self.password, "submit": "Login"}
+        with requests.Session() as s:
+            s.get('https://panel.cloudatcost.com', headers=headers)
+            s.post("https://panel.cloudatcost.com/manage-check2.php", headers=s.headers, data=payload)
+            soup_acount_number = BeautifulSoup(s.get("https://panel.cloudatcost.com").content, 'html5lib')
+            account_resources = dict()
+            for i in soup_acount_number.find_all("a", {"class": "btn btn-sm btn-default"}):
+                accountnumber = str(i).split('onclick="cloudpro(')[0].split('addNote("')[-1].split('-cloudpro-')[0].rsplit("c")[1]
+            soup_acount_resources = BeautifulSoup(s.get("https://panel.cloudatcost.com/panel/_config/pop/cloudpro.php?CNM={}".format(accountnumber)).content, 'html5lib')
+            for i in soup_acount_resources.find_all("table"):
+                for i in i.find_all("tr"):
+                    for i in i.find_all('td'):
+                        if "CPU" in str(i):
+                            totalcpus = str(i).split(" CPU:")[0].rsplit('">')[1]
+                        if "RAM" in str(i):
+                            totalram = str(i).split(" MB RAM:")[0].rsplit('">')[1]
+                        if " SSD:" in str(i):
+                            totalstorage = str(i).split(" SSD:")[0].rsplit('">')[1].split(" ")[0]
+                account_resources['Available'] = {"cpus": totalcpus, "ram": totalram, "storage": totalstorage}
+        return account_resources
 
-pprint(api("enter your username in here with the quates in tact",
-           "enter your password in here with the quates in tact").listservers())
+
+data = api("enter your email here but leave the quotes", "enter yourpassword in here but leave the quotes")
+pprint(data.listservers())
